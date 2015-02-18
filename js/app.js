5ae12329b4d2803f58a13b07ce3546250505c787
Rafael Perez <rapegoto@rafaels-air.home>
2015-02-18 12:16:22 +0100

    Add all files existing on february 18 2015

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$('#s4').cycle({ 
+	    fx:      'scrollUp', 
+	    speed:    2000, 
+	    timeout:  2000,
+	    pause:  1  
+	});
+
+	$(window).resize(function(){
+		$('#s4').css('height', ($('#s4').width() * 0.75));
+	});
+});
+
+
+// Possible effects:
+// blindX
+// blindY
+// blindZ
+// cover
+// curtainX
+// curtainY
+// fade
+// fadeZoom
+// growX
+// growY
+// scrollUp
+// scrollDown
+// scrollLeft
+// scrollRight
+// scrollHorz
+// scrollVert
+// shuffle
+// slideX
+// slideY
+// toss
+// turnUp
+// turnDown
+// turnLeft
+// turnRight
+// uncover
+// wipe
+// zoom
